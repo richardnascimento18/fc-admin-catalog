fd57283ac7ff7cddd4615b1a46ab038cf7cd572e
richardnascimento18 <computer.richard0923@gmail.com>
2026-08-08 15:00:24 -0300

    chore: add complete base project

--- infrastructure/src/main/java/com/fullcycle/admin/catalog/infrastructure/Main.java
+++ infrastructure/src/main/java/com/fullcycle/admin/catalog/infrastructure/Main.java
@@ -1,7 +1,11 @@
 package com.fullcycle.admin.catalog.infrastructure;
 
+import com.fullcycle.admin.catalog.application.UseCase;
+
 public class Main {
     public static void main(String[] args) {
         System.out.printf("Hello and welcome!");
+
+        System.out.println(new UseCase().execute());
     }
 }
