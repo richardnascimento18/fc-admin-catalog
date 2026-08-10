fa04772b4d25e3ff0adf9d6889258686820d3db6
richardnascimento18 <computer.richard0923@gmail.com>
2026-08-10 15:09:21 -0300

    feat(domain): finish category validator
    - Completed Unit tests to test business rules' constraints
    - Finished category validator

--- domain/src/main/java/com/fullcycle/admin/catalog/domain/category/Category.java
+++ domain/src/main/java/com/fullcycle/admin/catalog/domain/category/Category.java
@@ -31,11 +31,12 @@ public class Category extends AggregateRoot<CategoryID> {
         this.deletedAt = aDeletedDate;
     }
 
-    public static Category newCategory(final String aName, final String aDescription, final boolean anIsActive) {
+    public static Category newCategory(final String aName, final String aDescription, final boolean isActive) {
         final CategoryID id = CategoryID.unique();
         final Instant now = Instant.now();
+        final Instant deletedAt = isActive ? null : now;
 
-        return new Category(id, aName, aDescription, anIsActive, now, now, null);
+        return new Category(id, aName, aDescription, isActive, now, now, deletedAt);
     }
 
     @Override
--- domain/src/test/java/com/fullcycle/admin/catalog/domain/category/CategoryTest.java
+++ domain/src/test/java/com/fullcycle/admin/catalog/domain/category/CategoryTest.java
@@ -39,4 +39,87 @@ public class CategoryTest {
         Assertions.assertEquals(expectedErrorMessage, actualException.getErrors().get(0).message());
         Assertions.assertEquals(expectedErrorCount, actualException.getErrors().size());
     }
+
+    @Test
+    public void givenAnInvalidEmptyName_whenCallNewCategoryAndValidate_thenShouldReceiveError() {
+        final String expectedName = "  ";
+        final String expectedErrorMessage = "'name' should not be empty";
+        final int expectedErrorCount = 1;
+        final String expectedDescription = "The most watched category";
+        final boolean expectedIsActive = true;
+
+        final Category actualCategory = Category.newCategory(expectedName, expectedDescription, expectedIsActive);
+        final var actualException = Assertions.assertThrows(DomainException.class, () -> actualCategory.validate(new ThrowsValidationHandler()));
+
+        Assertions.assertEquals(expectedErrorMessage, actualException.getErrors().get(0).message());
+        Assertions.assertEquals(expectedErrorCount, actualException.getErrors().size());
+    }
+
+    @Test
+    public void givenAnInvalidNameLengthLessThan3Characters_whenCallNewCategoryAndValidate_thenShouldReceiveError() {
+        final String expectedName = "Fi ";
+        final String expectedErrorMessage = "'name' must be between 3 and 255 characters";
+        final int expectedErrorCount = 1;
+        final String expectedDescription = "The most watched category";
+        final boolean expectedIsActive = true;
+
+        final Category actualCategory = Category.newCategory(expectedName, expectedDescription, expectedIsActive);
+        final var actualException = Assertions.assertThrows(DomainException.class, () -> actualCategory.validate(new ThrowsValidationHandler()));
+
+        Assertions.assertEquals(expectedErrorMessage, actualException.getErrors().get(0).message());
+        Assertions.assertEquals(expectedErrorCount, actualException.getErrors().size());
+    }
+
+    @Test
+    public void givenAnInvalidNameLengthGreaterThan255Characters_whenCallNewCategoryAndValidate_thenShouldReceiveError() {
+        final String expectedName = "G6qPg6ymaqoyxTZLD2FV5rJDZHJWy2A9go2Fwif3ivoGSndEsy8R6vAIRRIfeJCQldLtAS6eBdlivusu2yTkfK7eH3nFYPM1thVrxT1XMsVfFbfoiCOPqmeoMWFZfCR3IMhCOUP7PyQMrSJi2X9rRHXB32AvUzTaBlQ7ADucKCXOWj0THTTM8lh4KHp8T7GCKLAzz0qgWohMWfJIMEotQJBwUTNeJoxObLj9vnujl5mu6lNcJOAMY0OI4JhZVntT";
+        final String expectedErrorMessage = "'name' must be between 3 and 255 characters";
+        final int expectedErrorCount = 1;
+        final String expectedDescription = "The most watched category";
+        final boolean expectedIsActive = true;
+
+        final Category actualCategory = Category.newCategory(expectedName, expectedDescription, expectedIsActive);
+        final var actualException = Assertions.assertThrows(DomainException.class, () -> actualCategory.validate(new ThrowsValidationHandler()));
+
+        Assertions.assertEquals(expectedErrorMessage, actualException.getErrors().get(0).message());
+        Assertions.assertEquals(expectedErrorCount, actualException.getErrors().size());
+    }
+
+    @Test
+    public void givenAValidEmptyDescription_whenCallNewCategoryAndValidate_thenShouldReceiveError() {
+        final String expectedName = "Filmes";
+        final String expectedDescription = "  ";
+        final boolean expectedIsActive = true;
+
+        final Category actualCategory = Category.newCategory(expectedName, expectedDescription, expectedIsActive);
+
+        Assertions.assertDoesNotThrow(() -> actualCategory.validate(new ThrowsValidationHandler()));
+        Assertions.assertNotNull(actualCategory);
+        Assertions.assertNotNull(actualCategory.getId());
+        Assertions.assertEquals(expectedName, actualCategory.getName());
+        Assertions.assertEquals(expectedDescription, actualCategory.getDescription());
+        Assertions.assertEquals(expectedIsActive, actualCategory.isActive());
+        Assertions.assertNotNull(actualCategory.getCreatedAt());
+        Assertions.assertNotNull(actualCategory.getUpdatedAt());
+        Assertions.assertNull(actualCategory.getDeletedAt());
+    }
+
+    @Test
+    public void givenAValidFalseIsActive_whenCallNewCategoryAndValidate_thenShouldReceiveError() {
+        final String expectedName = "Filmes";
+        final String expectedDescription = "The most watched category";
+        final boolean expectedIsActive = false;
+
+        final Category actualCategory = Category.newCategory(expectedName, expectedDescription, expectedIsActive);
+
+        Assertions.assertDoesNotThrow(() -> actualCategory.validate(new ThrowsValidationHandler()));
+        Assertions.assertNotNull(actualCategory);
+        Assertions.assertNotNull(actualCategory.getId());
+        Assertions.assertEquals(expectedName, actualCategory.getName());
+        Assertions.assertEquals(expectedDescription, actualCategory.getDescription());
+        Assertions.assertEquals(expectedIsActive, actualCategory.isActive());
+        Assertions.assertNotNull(actualCategory.getCreatedAt());
+        Assertions.assertNotNull(actualCategory.getUpdatedAt());
+        Assertions.assertNotNull(actualCategory.getDeletedAt());
+    }
 }
