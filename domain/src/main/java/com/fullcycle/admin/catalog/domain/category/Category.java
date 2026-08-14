e61eac6f3782bfc0fc63878189f81926bf40d2e3
richardnascimento18 <computer.richard0923@gmail.com>
2026-08-14 09:15:48 -0300

    feat(infrastructure): add Category Aggregate infrastructure implementations

--- domain/src/main/java/com/fullcycle/admin/catalog/domain/category/Category.java
+++ domain/src/main/java/com/fullcycle/admin/catalog/domain/category/Category.java
@@ -39,6 +39,18 @@ public class Category extends AggregateRoot<CategoryID> implements Cloneable {
         return new Category(id, aName, aDescription, isActive, now, now, deletedAt);
     }
 
+    public static Category with(
+            final CategoryID anId,
+            final String aName,
+            final String aDescription,
+            final boolean isActive,
+            final Instant aCreatedAt,
+            final Instant anUpdatedAt,
+            final Instant aDeletedAt
+    ) {
+        return new Category(anId, aName, aDescription, isActive, aCreatedAt, anUpdatedAt, aDeletedAt);
+    }
+
     @Override
     public void validate(final ValidationHandler handler) {
         new CategoryValidator(this, handler).validate();
---
+++ infrastructure/src/main/java/com/fullcycle/admin/catalog/infrastructure/category/CategoryMySQLGateway.java
@@ -0,0 +1,45 @@
+package com.fullcycle.admin.catalog.infrastructure.category;
+
+import com.fullcycle.admin.catalog.domain.category.Category;
+import com.fullcycle.admin.catalog.domain.category.CategoryGateway;
+import com.fullcycle.admin.catalog.domain.category.CategoryID;
+import com.fullcycle.admin.catalog.domain.category.CategorySearchQuery;
+import com.fullcycle.admin.catalog.domain.pagination.Pagination;
+import com.fullcycle.admin.catalog.infrastructure.category.persistence.CategoryRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class CategoryMySQLGateway implements CategoryGateway {
+    private final CategoryRepository categoryRepository;
+
+    public CategoryMySQLGateway(final CategoryRepository aCategoryRepository) {
+        this.categoryRepository = aCategoryRepository;
+    }
+
+    @Override
+    public Category create(Category aCategory) {
+        return null;
+    }
+
+    @Override
+    public void deleteById(CategoryID anId) {
+
+    }
+
+    @Override
+    public Optional<Category> findById(CategoryID anId) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Category update(Category aCategory) {
+        return null;
+    }
+
+    @Override
+    public Pagination<Category> findAll(CategorySearchQuery aQuery) {
+        return null;
+    }
+}
